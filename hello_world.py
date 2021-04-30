6452d76fcf50a1b3388a12a96b6de20f7169586a
Tanmaybhujade <tanmaybhujade@gmail.com>
2021-05-01 00:39:59 +0530

    3rd day work

--- hello_world.py
+++ hello_world.py
@@ -11,3 +11,12 @@ print("Hello,world!")
 #this  program prints Hello, world!
 
 print("Hello,world!")
+
+#this  program prints Hello, world!
+
+print("Hello,world!")
+
+
+#this  program prints Hello, world!
+
+print("Hello,world!")
